967ea53c482e2e406c429c62c0d1d6047efcda22
vloydd <ma7kitan@gmail.com>
2021-10-17 23:12:41 +0300

    the end of 17.10.21

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-const browserSync = require('browser-sync').create();
 
 
 exports.style = style;      
@@ -11,13 +10,7 @@ exports.watch = watch;
         return gulp.src('./scss/**/*.scss')
         .pipe(sass().on('error' ,sass.logError))
         .pipe(gulp.dest('./css/'))
-        .pipe(browserSync.stream());
     }   
     function watch() {
-        browserSync.init({
-            server: {
-                baseDir: './'
-            }
-        });
         gulp.watch('./scss/**/*.scss', style);
     }
